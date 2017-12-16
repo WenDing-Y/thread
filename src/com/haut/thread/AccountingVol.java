e982b6466d3ed750e37c52cf4ba5d43fbedca88c
iareuniqe <1062698930@qq.com>
2017-12-16 16:52:47 +0800

    format code

--- src/com/haut/thread/AccountingVol.java
+++ src/com/haut/thread/AccountingVol.java
@@ -4,5 +4,30 @@ package com.haut.thread;
  * @author shuiyu_lei
  * @date 2017/12/16
  */
-public class AccountingVol {
+public class AccountingVol implements Runnable {
+
+    static AccountingVol accountingVol = new AccountingVol();
+    static volatile int i = 0;
+
+    public static void increase() {
+        i++;
+    }
+
+
+    @Override
+    public void run() {
+        for (int j = 0; j < 1000; j++) {
+            increase();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(accountingVol);
+        Thread thread1 = new Thread(accountingVol);
+        thread.start();
+        thread1.start();
+        thread.join();
+        thread1.join();
+        System.out.println(i);
+    }
 }
